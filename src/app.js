ceecf3ef8912155badbe0834465fc1eedf14accf
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-29 22:40:21 +0100

    updates todo array to take objects, and adds radio buttons to track the level of priority

--- src/app.js
+++ src/app.js
@@ -4,12 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
-      items: ["Finish Homework", "Clean Car", "Go Shopping"],
-      newTodo: ""
+      items: [
+        { name: "Finish Homework", priority: "High"},
+        { name: "Clean Car", priority: "Low"},
+        { name: "Tidy House", priority: "Low"},
+        { name: "Catch up on Game of Thrones", priority: "High"}
+      ],
+      newTodo: "",
+      picked: ""
     },
     methods: {
       saveNewTodo: function(){
-        this.items.push(this.newTodo);
+        this.items.push({
+          name: this.newTodo,
+          priority: this.picked
+        });
         this.newTodo = "";
       }
     }
